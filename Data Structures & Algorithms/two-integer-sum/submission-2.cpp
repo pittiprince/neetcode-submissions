1fab607de6da751b4c70b2a42db969dcc696f90d
Pitti Prince Charles <117570887+pittiprince@users.noreply.github.com>
2026-08-18 22:51:24 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hash;
+        for(int i = 0; i<nums.size(); i++){
+            int key = target - nums[i] ;
+            if(hash.find(key) != hash.end()){
+                return{hash.find(key)->second,i};
+            }
+            hash.insert({nums[i],i});
+        }
+        return {};
+    }
+};
